d97df2d10ac6c900181fed01658af856c4d56a41
mpenajoia <penajoia.marco@gmail.com>
2022-07-14 10:13:09 -0700

    loader written into webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -8,4 +8,16 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].js',
   },
+  module: {
+    rules: [{ 
+        test: /\.(js|jsx)$/, 
+        exclude: /node_modules/,
+        use: {
+            loader: 'babel-loader',
+            options: {
+                presets: ['@babel/preset-env', '@babel/preset-react'],
+            }
+        } 
+    }],
+  },
 };
