d400e990c008172225b9bf16a8231961d0b7bc52
mpenajoia <penajoia.marco@gmail.com>
2022-09-23 10:20:50 -0700

    Createpassword comp

--- src/components/setup/createPassword/CreatePassword.js
+++ src/components/setup/createPassword/CreatePassword.js
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function CreatePassword() {
+
   return (
     <div className='text-center'>
         <h2 className='text-2xl text-center mb-5 font-medium'>Create a password</h2>
         <p className='font-medium text-lg'>You will use this to unlock your wallet</p>
+        <form>
+            <input type="password" name="password" />
+            <input type="password" name="confirm" />
+        </form>
     </div>
   )
 }
