699d3898179ebefbb4379d2bd68b946bb4e1e9ca
mpenajoia <penajoia.marco@gmail.com>
2022-09-28 11:13:13 -0700

    Checkbox and back button

--- src/components/setup/CreatePassword.js
+++ src/components/setup/CreatePassword.js
@@ -26,8 +26,13 @@ function CreatePassword() {
     
 
   return (
-    <div className='text-center w-full flex flex-col justify-between items-stretch'>
-        <button>&#60;</button>
+    <div className='text-center w-full flex flex-col justify-between items-stretch relative'>
+        <div className='absolute h-[20px] w-[20px] top-[-70px] p-[1px] rounded-sm bg-gradient-to-r from-[#E4FC70] to-[#9AECA4]'>
+            <div className='bg-[#37393F] rounded-sm'>
+                <button className='text-transparent bg-clip-text bg-gradient-to-r from-[#E4FC70] to-[#9AECA4]'>&#60;</button>
+            </div>
+        </div>
+
         <h2 className='text-2xl text-center mb-5 font-medium'>Create a password</h2>
         <p className='font-medium text-base'>You will use this to unlock your wallet</p>
         <form className='flex flex-col items-center w-full text-[#F4F2EE]/[0.5] gap-4 mt-10'>
@@ -40,6 +45,14 @@ function CreatePassword() {
                 <div className='w-5/6'>
                     <p className={`${match ? 'opacity-0' : 'opacity-1' } cursor-default text-left text-[#FF7171]`}>Passwords do not match</p>
                 </div>
+            </div>
+            
+            <div className="flex items-center font-normal text-sm text-white">
+                <input id="link-checkbox" type="checkbox" className="w-4 h-4 accent-[#37393F] bg-[#37393F] rounded border border-[#F4F2EE] "/>
+
+                <label for="link-checkbox" className="ml-2">I agree to 
+                    <a href="#" target="_blank" rel="noopener" className="text-[#E4FC70] underline"> Terms of Service</a>
+                </label>
             </div>
         </form>
         
